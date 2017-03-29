bb66f209c9d60fcebf5e77144468b4051d023ff0
Estres <Estres@192.168.40.141>
2017-03-29 13:57:39 -0300

    Update!

--- src/test/java/test/resources/TestSuite.java
+++ src/test/java/test/resources/TestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ Test_Reporte_HTML.class,Test_Reporte_LIB.class,TrackFeature.class,
+@SuiteClasses({ /*Test_Reporte_HTML.class,Test_Reporte_LIB.class,*/TrackFeature.class,
 	AboutFeature.class,BookFeature.class
 	
 })
